fca0126662e07e35df05a6a6f22fec998cf56714
Finn O'Donoghue <finnodonoghue@gmail.com>
2023-07-24 03:38:17 +1000

    Refactor CLI to run a specific day challenge

--- src/challenges.rs
+++ src/challenges.rs
@@ -5,10 +5,23 @@ pub mod day2b;
 pub mod day3a;
 pub mod day3b;
 pub mod day4a;
-// pub mod day4b;
-// pub mod day5a;
+pub mod day4b;
+pub mod day5a;
 // pub mod day5b;
 // pub mod day6a;
 // pub mod day6b;
 // pub mod day7a;
 // pub mod day7b;
+
+pub const ALL_ANSWERS: [fn(&str) -> String; 9] = [
+    day1a::answer,
+    day1b::answer,
+    day2a::answer,
+    day2b::answer,
+    day3a::answer,
+    day3b::answer,
+    day4a::answer,
+    day4b::answer,
+    day5a::answer,
+    // day5::answer,
+];
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,47 @@
-#![allow(dead_code)]
-
+#![allow(dead_code, unused_variables)]
 use std::fs;
+use std::env;
+
 mod challenges;
 
 
 fn main() {
-    let file_path = "src/input/day4.txt";
+    let args: Vec<_> = env::args().skip(1).collect();
 
-    let contents: String = fs::read_to_string(file_path).unwrap();
-    let answer = challenges::day4a::answer(&contents);
-    println!("{answer}")
+    let err_str = "Provide a day argument like 5a";
+    let command = args.get(0).expect(err_str);
+    let (day, a_b) = extract_xy(command).expect(err_str);
+
+    let file_path = format!("src/input/day{}.txt", day);
+    let contents: String = fs::read_to_string(file_path).expect(&format!("Couldn't find src/input/day{}.txt", day));
+
+    let answer_index = (2*(day - 1) + match a_b {
+        'a' => 0,
+        'b' => 1,
+        _ => panic!("Day must be either xa or xb")
+    }) as usize;
+
+    let answer = challenges::ALL_ANSWERS[answer_index](&contents);
+    println!("Day {day} part {a_b}\n{answer}")
+}
+
+fn extract_xy(element: &str) -> Option<(u32, char)> {
+    let chars: Vec<char> = element.chars().collect();
+    if chars.len() != 2 {
+        return None;
+    }
+
+    let x_char = chars[0];
+    let y_char = chars[1];
+
+    if y_char != 'a' && y_char != 'b' {
+        return None;
+    }
+
+    let x: u32 = x_char.to_digit(10)?;
+    if x < 1 || x > 25 {
+        return None;
+    }
+
+    Some((x, y_char))
 }
